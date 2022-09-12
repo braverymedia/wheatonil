d59c2dfd07c8e8b8b9779ac425463fafee1e152b
Joel Goodman <joel@bravery.co>
2022-09-12 16:58:17 -0500

    nav and footer

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+import path from 'path';
+const config = require('./bravery.config.js');
+
+const ASSETS_DIR = config.dir.assets;
+const DIST_DIR = config.dir.dist;
+
+const JS_SRC = path.join(ASSETS_DIR, 'js');
+const JS_DIST = path.join(DIST_DIR, 'assets/js');
+
+export default {
+    input: path.join(JS_SRC, 'wheaton.js'),
+    output: {
+        file: path.join(JS_DIST, 'wheaton.bundle.js'),
+        format: 'iife'
+    },
+    plugins: [
+        resolve(),
+		commonjs(),
+		terser()
+    ]
+}
