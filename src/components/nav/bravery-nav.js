484b901ffe765fcda0f61ebbec033c4620b7362f
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2022-09-23 10:08:42 -0400

    added search suggestion panel

--- src/components/nav/bravery-nav.js
+++ src/components/nav/bravery-nav.js
@@ -1,12 +1,10 @@
 const showOverlay = () => {
     // const mobileMenu = document.getElementById("menu-overlay-content-wrapper");
     const overlay = document.getElementById("menu-overlay-content-wrapper")
-    console.log("clicked", overlay)
     overlay.style.height = "100%";
 }
 
 const closeOverlay = () => {
-    console.log("clicked x")
     const overlay = document.getElementById("menu-overlay-content-wrapper")
     overlay.style.height = "0%";
 }
--- src/components/overlay/bravery-overlay.js
+++ src/components/overlay/bravery-overlay.js
@@ -5,3 +5,19 @@ const getCurrentYearOverlay = () => {
 var year_ref = document.getElementById("copyright-year-overlay");
 
 year_ref.innerHTML = getCurrentYearOverlay();
+
+const showSearchSuggestion = () => {
+    //TODO - add closeShowSuggestion() logic when user clicks away/out of the input
+    //TODO add nav element around .overlay-content-main
+    var search_panel = document.getElementById("search-suggestion-panel");
+    console.log(search_panel.style)
+
+    if (search_panel.style.display === "none") {
+        search_panel.style.display = "flex";
+        search_panel.style.flexDirection = "column"
+        console.log("make flex direction column")
+    } else {
+        search_panel.style.display = "flex";
+    }
+}
+
