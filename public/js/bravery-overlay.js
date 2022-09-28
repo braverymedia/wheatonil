c3ca0dee846e2c2954fbf025c29920ed11e6da19
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2022-09-28 16:20:38 -0400

    made search suggestion panel position absolute and added opacity to overlay content links

--- public/js/bravery-overlay.js
+++ public/js/bravery-overlay.js
@@ -6,21 +6,31 @@ var year_ref = document.getElementById("copyright-year-overlay");
 year_ref.innerHTML = getCurrentYearOverlay();
 
 var search_panel = document.getElementsByClassName("hide-search-suggestion-panel");
-var search_panel_div = search_panel[0]
+var search_panel_div = search_panel[0];
+
+var overlay_content_wrapper = document.getElementsByClassName("overlay-content-wrapper");
+var overlay_content_wrapper_div = overlay_content_wrapper[0];
 
 //listener for click-away from search bar
 window.onclick = () => {
     if (search_panel_div.classList.contains("show-search-suggestion-panel")) {
         search_panel_div.classList.remove("show-search-suggestion-panel");
         search_panel_div.classList.add("hide-search-suggestion-panel")
+
+        overlay_content_wrapper_div.classList.remove("hide-overlay-content-wrapper")
+        overlay_content_wrapper_div.classList.add("overlay-content-wrapper")
+
     }
 }
 
 const showSearchSuggestion = (event) => {
     if (search_panel_div.classList.contains("hide-search-suggestion-panel")) {
         search_panel_div.classList.remove("hide-search-suggestion-panel");
-        search_panel_div.classList.add("show-search-suggestion-panel")
-        console.log(search_panel_div)
+        search_panel_div.classList.add("show-search-suggestion-panel");
+
+        overlay_content_wrapper_div.classList.remove("overlay-content-wrapper")
+        overlay_content_wrapper_div.classList.add("hide-overlay-content-wrapper")
+        
     } 
     event.stopPropagation()
 };
