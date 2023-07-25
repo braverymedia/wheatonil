11d035d54a9268e71ef9209057445adbeb92f257
Jason Woodward <jason@jwoodward.com>
2023-07-25 10:30:29 -0400

    sort by Name, alphabetically

--- src/_includes/assets/js/ProgramFinder.svelte
+++ src/_includes/assets/js/ProgramFinder.svelte
@@ -82,34 +82,40 @@
   }
   $: {
     /* compute which programs to show */
-    programs_filtered = programs.filter((program) => {
-      let pass = true;
-      if (selected_areas_of_study.length > 0) {
-        /* if there are selected areas of study, and the program has areas of study specified, conditionally show it */
-        pass = false;
-        if (program.areas_of_study) {
-          program.areas_of_study.forEach((aos) => {
-            if (selected_areas_of_study.includes(aos)) {
-              pass = true;
-            }
-          });
+    programs_filtered = programs
+      .filter((program) => {
+        let pass = true;
+        if (selected_areas_of_study.length > 0) {
+          /* if there are selected areas of study, and the program has areas of study specified, conditionally show it */
+          pass = false;
+          if (program.areas_of_study) {
+            program.areas_of_study.forEach((aos) => {
+              if (selected_areas_of_study.includes(aos)) {
+                pass = true;
+              }
+            });
+          }
+          if (!pass) return pass;
         }
-        if (!pass) return pass;
-      }
-      if (selected_credential_types.length > 0) {
-        /* if there are selected credential types, and the program has credential types specified, conditionally show it */
-        pass = false;
-        if (program.credential_types) {
-          program.credential_types.forEach((ct) => {
-            if (selected_credential_types.includes(ct)) {
-              pass = true;
-            }
-          });
+        if (selected_credential_types.length > 0) {
+          /* if there are selected credential types, and the program has credential types specified, conditionally show it */
+          pass = false;
+          if (program.credential_types) {
+            program.credential_types.forEach((ct) => {
+              if (selected_credential_types.includes(ct)) {
+                pass = true;
+              }
+            });
+          }
+          if (!pass) return pass;
         }
-        if (!pass) return pass;
-      }
-      return pass;
-    });
+        return pass;
+      })
+      .sort((a, b) => {
+        return (a.name || "")
+          .toLowerCase()
+          .localeCompare((b.name || "").toLowerCase());
+      });
   }
   function toggleCredentialType(t) {
     if (selected_credential_types.includes(t)) {
