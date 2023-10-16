ec2f252ec6eb2d1da09498a85f5acf7cd3249546
Jason Woodward <jason@jwoodward.com>
2023-10-16 15:19:14 -0400

    exclude incomplete programs and their data from program finder (#11)
    * v1 of Program Finder
    
    * introduce nix flake for dev env
    * enable dev web server in 11ty config
    * introduce svelte build capability
    
    * update README to reflex possibility of using nix flake for dev env setup
    
    * sort by Name, alphabetically
    
    * mobile responsive views
    
    * extra classes for if you want to style outside the component
    
    * add fancy CSS checkboxes
    
    * fix checkbox alignment
    
    * accommodate ordered credential types by looking at number suffixes
    
    * sort areas by alpha
    
    * rename in prep for slide-in refactor
    
    * include link to the program page
    
    * split out Program Finder Search form into its own module in prep for making overlay
    
    * componentize sample data and pull in PF js everywhere instead of just on home page
    
    * PF in overlay or in full screen, linking from overlay to fullscreen, full forward/back button and deep link support
    
    * move demo from /programfinder/ to /programs/ to match what's gonna go on prod
    
    * fixup busted code
    
    * make new CSS for checkbox look good on white background by default
    
    * refactor deciding when to pushState
    
    * bring pnpm lock inline with npm lockfile and current state of package.json
    
    * don't show incomplete programs, or use attributes of those programs to construct program finder fields
    
    * update sample data to demonstrate new "incomplete program data" filtering

--- src/assets/js/program-finder-2023.js
+++ src/assets/js/program-finder-2023.js
@@ -1,28 +1,34 @@
-
 import ProgramFinderFullScreen from "./ProgramFinderFullScreen.svelte";
 import ProgramFinderOverlay from "./ProgramFinderOverlay.svelte";
+import { isShowableProgram } from "./program-finder-utils";
 window.addEventListener("DOMContentLoaded", (event) => {
   /* here's where we get the data elements written to the DOM by the T4 nav system */
   const data_elements = document.getElementsByClassName("program-data-2023");
   /* this prepares the array of Programs passed into the ProgramFinder component.  It's set up to expect a | as a delimiter, and expects data-name, data-credential-types, data-degree-types and data-areas-of-study as attributes on the .program-data-2023 divs */
-  const programs = Array.prototype.map.call(data_elements, (element) => {
-    return {
-      name: element.dataset.name || "Unnamed",
-      credential_types: (element.dataset.credentialTypes || "")
-        .split("|")
-        .map((e) => e.trim()),
-      credential_type_values: (element.dataset.credentialTypeValues || "")
-        .split("|")
-        .map((e) => e.trim()),
-      degree_types: (element.dataset.degreeTypes || "")
-        .split("|")
-        .map((e) => e.trim()),
-      areas_of_study: (element.dataset.areasOfStudy || "")
-        .split("|")
-        .map((e) => e.trim()),
-      link: element.dataset.link || "",
-    };
-  });
+  const programs = Array.prototype.map
+    .call(data_elements, (element) => {
+      return {
+        name: element.dataset.name || "Unnamed",
+        credential_types: (element.dataset.credentialTypes || "")
+          .split("|")
+          .map((e) => e.trim())
+          .filter((e) => !!e),
+        credential_type_values: (element.dataset.credentialTypeValues || "")
+          .split("|")
+          .map((e) => e.trim())
+          .filter((e) => !!e),
+        degree_types: (element.dataset.degreeTypes || "")
+          .split("|")
+          .map((e) => e.trim())
+          .filter((e) => !!e),
+        areas_of_study: (element.dataset.areasOfStudy || "")
+          .split("|")
+          .map((e) => e.trim())
+          .filter((e) => !!e),
+        link: element.dataset.link || "",
+      };
+    })
+    .filter(isShowableProgram);
   const fullscreen_mountpoint = document.getElementById(
     "program-finder-mountpoint"
   );
