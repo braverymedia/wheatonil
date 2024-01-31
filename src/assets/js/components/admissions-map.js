8498b6083066dc30e3082d6dd37b97acf2c76859
Joel Goodman <joel@braverymedia.co>
2024-01-31 14:42:06 -0600

    map filter

--- src/assets/js/components/admissions-map.js
+++ src/assets/js/components/admissions-map.js
@@ -1,43 +1,40 @@
 // AI Try
 document.addEventListener("DOMContentLoaded", () => {
+	const repMapItem = document.querySelectorAll(".rep__map__item");
+	const repNavSelect = document.querySelector(".rep__select");
 	const filterReps = (filterText) => {
-		document.querySelector(".rep__result").innerHTML = "";
+		Array.from(
+			document.querySelector(".filterable-group").children
+		).forEach((item) => {
+			const itemFilterStr = item.getAttribute("data-region");
+			if (!itemFilterStr) return;
 
-		const showRep = Array.from(
-			document.querySelector(".rep-profile__group").children
-		)
-			.filter((item) => {
-				const itemFilterStr = item.getAttribute("data-region");
-				if (!itemFilterStr) return false;
+			const itemFilter = itemFilterStr
+				.split(/,/)
+				.map((i) => i.trim());
 
-				const itemFilter = itemFilterStr
-					.split(/,/)
-					.map((i) => i.trim());
-
-				return itemFilter && itemFilter.indexOf(filterText) >= 0;
-			})
-			.map((item) => item.cloneNode(true));
-
-		showRep.forEach((item) => {
-			document.querySelector(".rep__result").appendChild(item);
+			if (itemFilter && itemFilter.indexOf(filterText) >= 0) {
+				item.setAttribute("aria-hidden", "false");
+			} else {
+				item.setAttribute("aria-hidden", "true");
+			}
 		});
 	};
 
-	const repNavSelect = document.querySelector(".rep__select");
 	repNavSelect.addEventListener("change", () => {
 		const selectVal = repNavSelect.value;
 		filterReps(selectVal);
-		repMapItem.classList.remove("selected");
 
 		Array.from(repMapItem).forEach((item) => {
 			const itemNode = item;
 			if (itemNode.id == selectVal) {
 				itemNode.classList.add("selected");
+			} else {
+				itemNode.classList.remove("selected");
 			}
 		});
 	});
 
-	const repMapItem = document.querySelectorAll(".rep__map__item");
 	repMapItem.forEach((item) => {
 		item.addEventListener("click", () => {
 			const clickVal = item.id;
